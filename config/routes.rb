b52b93dded44721505045709f7c0b50cf83b5fca
Terrence <chongweijie88@MacBook-Pro-2.local>
2016-08-30 11:17:49 +0800

    set up admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'static_pages#home'
   get :about, to: 'static_pages#about'
   get :faq, to: 'static_pages#FAQ'
